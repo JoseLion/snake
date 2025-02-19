3f0d2c0d3ad4d39290c088522d91cdb339fb7283
Jose Luis Leon <joseluis5000l@gmail.com>
2025-02-18 21:50:21 -0500

    feat(core): Create food

---
+++ src/layout/grid.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace Grid {
+  
+  static constexpr int CELL_SIZE = 30;
+  static constexpr int CELL_COUNT = 25;
+  static constexpr int SIZE = CELL_COUNT * CELL_SIZE;
+
+  int randomCell();
+
+  int cellOrigin(const int& cell);
+}
---
+++ src/layout/theme.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "raylib.h"
+
+namespace Theme {
+
+  static constexpr Color GREEN_500 = Color { 173, 204, 96, 255 };
+  static constexpr Color GREEN_900 = Color { 43, 51, 24, 255 };
+}
